4b839ae759b27e47b1a8bd72b4cf6d04f2687199
CzerwonyV <victor.veyrier@icloud.com>
2020-05-23 05:26:15 +0200

    Correct bug in Responsive

---
+++ build/precache-manifest.edf1a7d42b0022541d1cc1381e9a20b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e291cecf65014a8f777b00f8449c26db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88ba43ed5b2a0395854b",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "bf2c87c141d1caf728ef",
+    "url": "/static/css/main.371f87d2.chunk.css"
+  },
+  {
+    "revision": "88ba43ed5b2a0395854b",
+    "url": "/static/js/2.9c1a5d2a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9c1a5d2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf2c87c141d1caf728ef",
+    "url": "/static/js/main.521f235c.chunk.js"
+  },
+  {
+    "revision": "8000db6c4fdde04a8741",
+    "url": "/static/js/runtime-main.7c3b42f0.js"
+  }
+]);
